fef0a287dcd6453812272d61bbd9b54b0cf3b8db
TedMdelaCruz <tedmdelacruz@gmail.com>
2013-06-30 21:59:48 +0800

    Moved controller/method/args resolution from Application::start() to Router::resolve()

--- system/core/Application.php
+++ system/core/Application.php
@@ -1,54 +1,43 @@
-<?php namespace Core;
-
+<?php
 /**
  *
  * Core MVC Application
+ *
+ * The Application is also an IoC container, similar to Laravel
  *
  * @package     Core
  * @author      Ted Mathew dela Cruz
  * @copyright   Copyright (c) 2013 - 2014, Ted Mathew dela Cruz.
  * @link        http://core.tedmdelacruz.com
  *
- * TODO: Form Validation
- *
  */
 
-// The Core app is also an IoC container, similar to Laravel
+namespace Core;
+
 class Application extends IoC
 {
     /**
      * Start the Core
+     * @return void
      */
     function start() {
 
-        try
+        // Connect to the database if is set
+        if( is_object($this->db) )
         {
-            // Retrieve the current controller and method
-            $controller = $this->router->getController();
-            $method = $this->router->getMethod();
-
-            // Connect to the database if is set
-            if( is_object($this->db) )
-            {
-                $this->db->connect();
-            }
-
-            // Try instantiating the controller and calling the method
-            if( ! class_exists($controller) OR ! method_exists($controller, $method)) {
-                throw new Exceptions\CoreException("Page not found", 404);
-            }
-
-            $app_controller = new $controller();
-            $app_controller->$method();
+            $this->db->connect();
         }
-        catch(\Exception $e)
-        {
-            $error['code'] = $e->getCode();
-            $error['message'] = $e->getMessage();
 
-            HTTP::status_code($e->getCode());
+        // Attempt to resolve the current route
+        $this->router->resolve();
+    }
 
-            View::render('error', $error);
-        }
+    /**
+     * Stops the Core
+     * @return void
+     */
+    function stop()
+    {
+        $this->session->clearFlashData();
     }
 }
