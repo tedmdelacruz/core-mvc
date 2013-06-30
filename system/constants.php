f6919d569fae8df9c595f46160176d4de67658d5
TedMdelaCruz <tedmdelacruz@gmail.com>
2013-06-30 22:08:18 +0800

    Added spaces between concatenations

--- system/constants.php
+++ system/constants.php
@@ -7,19 +7,19 @@
 */
 define('BASE_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/core/public/'); // I'm not sure about this
 
-define('CORE_CLASSES_DIR', SYS_PATH.'core/');
+define('CORE_CLASSES_DIR', SYS_PATH . 'core/');
 
 /*
 |--------------------------------------------------------------------------
 | Application Constants
 |--------------------------------------------------------------------------
 */
-define('CONTROLLERS_DIR', APP_PATH.'controllers/');
+define('CONTROLLERS_DIR', APP_PATH . 'controllers/');
 
-define('CONFIG_DIR', APP_PATH.'config/');
+define('CONFIG_DIR', APP_PATH . 'config/');
 
-define('MODELS_DIR', APP_PATH.'models/');
+define('MODELS_DIR', APP_PATH . 'models/');
 
-define('LIBRARIES_DIR', APP_PATH.'libraries/');
+define('LIBRARIES_DIR', APP_PATH . 'libraries/');
 
-define('VIEWS_DIR', APP_PATH.'views/');
+define('VIEWS_DIR', APP_PATH . 'views/');
