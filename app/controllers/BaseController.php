2bdd97c7ec30736944ffcf626e1eb55a16b3b9eb
TedMdelaCruz <tedmdelacruz@gmail.com>
2013-06-30 22:10:45 +0800

    Used View::set() for setting view data from BaseController

--- app/controllers/BaseController.php
+++ app/controllers/BaseController.php
@@ -1,9 +1,9 @@
-<?php if ( ! defined('SYS_PATH')) exit('No direct script access allowed');
+<?php
 /**
  * Base Controller
  *
  */
-class BaseController extends Controller
+class BaseController extends Core\Controller
 {
     /**
      * Base data to be passed to the view
@@ -22,11 +22,18 @@ class BaseController extends Controller
         Asset::add('master', 'assets/css/master.css');
         Asset::add('app', 'assets/js/app.js');
 
-        $this->data['user'] = Auth::user();
+        // $this->data['user'] = Auth::user();
 
         // Prepare the <head> data
         $this->data['title']       = "Core MVC";
         $this->data['author']      = "Ted Mathew dela Cruz";
         $this->data['description'] = "PHP micro MVC framework experiment";
+
+        // Prepare alert messages
+        $this->data['error']   = '';
+        $this->data['success'] = '';
+        $this->data['notice']  = '';
+
+        View::set($this->data);
     }
 }
