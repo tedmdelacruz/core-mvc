6aa1e0aa62b47e1a424bf063909b58939b8ce97c
TedMdelaCruz <tedmdelacruz@gmail.com>
2013-06-30 22:05:34 +0800

    Massive overhaul for Session

--- system/core/Session.php
+++ system/core/Session.php
@@ -1,4 +1,4 @@
-<?php namespace Core;
+<?php
 /**
  *
  * Session
@@ -8,6 +8,9 @@
  * @copyright   Copyright (c) 2013 - 2014, Ted Mathew dela Cruz.
  * @link        http://core.tedmdelacruz.com
  */
+
+namespace Core;
+
 class Session
 {
     public function __construct()
@@ -20,17 +23,35 @@ class Session
      * @param string $identifier Session identifier
      * @param mixed  $value      Session value
      */
-    public static function set_data($identifier, $value)
+    public function setData($identifier, $value)
     {
         $_SESSION[$identifier] = $value;
     }
 
-    public static function stop()
+    /**
+     * Stops the current session
+     * @return void
+     */
+    public function stop()
     {
         session_stop();
     }
 
-    public static function get_data($identifier)
+    /**
+     * Clears all Session data
+     * @return void
+     */
+    public function clear()
+    {
+        session_unset();
+    }
+
+    /**
+     * Retrieves a Session data
+     * @param  string $identifier
+     * @return mixed
+     */
+    public function getData($identifier)
     {
         if(self::is_set($identifier))
         {
@@ -40,9 +61,15 @@ class Session
         return false;
     }
 
-    public static function is_set($identifier)
+    /**
+     * Determines if a Session is set.
+     * Supposedly named isSet but isSet is a reserved keyword
+     * @param  string  $identifier
+     * @return boolean
+     */
+    public function isRegistered($identifier)
     {
-        if(isset($_SESSION[$identifier]))
+        if( isset($_SESSION[$identifier]) )
         {
             return true;
         }
@@ -50,4 +77,38 @@ class Session
         return false;
     }
 
+    /**
+     * Sets a Session flash data
+     * @param string $identifier
+     * @param string $message
+     */
+    public function setFlashData($identifier, $message)
+    {
+        $_SESSION['FLASH_DATA'][$identifier] = $message;
+    }
+
+    /**
+     * Gets a Session flash data
+     * Removes the Session data on retrieval
+     * @param  string $identifier
+     * @return string mixed
+     */
+    public function getFlashData($identifier)
+    {
+        if( isset($_SESSION['FLASH_DATA'][$identifier]) )
+        {
+            return $_SESSION['FLASH_DATA'][$identifier];
+        }
+
+        return NULL;
+    }
+
+    /**
+     * Clears Session flash data
+     * @return void
+     */
+    public function clearFlashData()
+    {
+        $_SESSION['FLASH_DATA'] = array();
+    }
 }
